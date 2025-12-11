42817ffe6ad2819140de4a6ce6ce62000d59ad0e
Хафизов Георгий Игоревич <84989833+Geortor@users.noreply.github.com>
2025-12-11 13:10:57 +0300

    Create email_checker.py

---
+++ email_checker.py
@@ -0,0 +1,41 @@
+import dns.resolver
+
+
+def check_mx_records(emails):
+    print(f"{'Email':<30} | {'Статус'}")
+    print("-" * 60)
+
+    for email in emails:
+        if '@' not in email:
+            print(f"{email:<30} | Некорректный формат email")
+            continue
+
+        domain = email.split('@')[-1]
+
+        try:
+            # найти MX-записи для домена
+            records = dns.resolver.resolve(domain, 'MX')
+
+            status = "домен валиден"
+
+        except dns.resolver.NXDOMAIN:
+            status = "домен отсутствует"
+
+        except (dns.resolver.NoAnswer, dns.resolver.NoNameservers, dns.resolver.LifetimeTimeout):
+            status = "MX-записи отсутствуют или некорректны"
+
+        except Exception:
+            status = "MX-записи отсутствуют или некорректны"
+
+        print(f"{email:<30} | {status}")
+
+
+email_list = [
+    "user@google.com",
+    "test@yandex.ru",
+    "fake@nonexistent123.com",
+    "user@example.com",
+    "admin@localhost"
+]
+
+check_mx_records(email_list)
